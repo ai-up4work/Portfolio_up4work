17f6c049e2e74e2e057a6a679baae48f737f028b
SafnasKaldeen <safnaskm.20@uom.lk>
2026-01-24 12:20:55 +0530

    sad

--- next.config.mjs
+++ next.config.mjs
@@ -10,30 +10,24 @@ const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.google.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "unsplash.com",
-        pathname: "**",
-      },
-      // Add Cloudinary for gallery and CMS images
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "**",
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "alzia.vercel.app",
+      pathname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      pathname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      pathname: "**",
+    },
+  ],
+},
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
