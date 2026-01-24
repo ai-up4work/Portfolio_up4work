0372303fd5f67393ebea2c4f94edfd852b9d7718
SafnasKaldeen <safnaskm.20@uom.lk>
2026-01-24 18:46:50 +0530

    share

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import type {
 import { home } from "./index"
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com"
+const baseURL: string = "https://www.up4work.space"
 
 const routes: RoutesConfig = {
   "/": true,
@@ -207,10 +207,10 @@ const socialSharing: SocialSharingConfig = {
   platforms: {
     x: true,
     linkedin: true,
-    facebook: false,
+    facebook: true,
     pinterest: false,
-    whatsapp: false,
-    reddit: false,
+    whatsapp: true,
+    reddit: true,
     telegram: false,
     email: true,
     copyLink: true,
